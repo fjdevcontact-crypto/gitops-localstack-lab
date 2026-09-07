df6874c6226226e81535f7817d28e5d520603dc7
fjdevcontact-crypto <fjdevcontact@gmail.com>
2026-09-06 22:34:42 -0300

    feat: add localstack infrastructura module

---
+++ infrastructure/localstack/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  s3_use_path_style = true
+
+  endpoints {
+    s3       = "http://localstack:4566"
+    dynamodb = "http://localstack:4566"
+  }
+}
+
+resource "aws_s3_bucket" "controller_lab" {
+  bucket = "devops-lab-tofu-controller"
+}
